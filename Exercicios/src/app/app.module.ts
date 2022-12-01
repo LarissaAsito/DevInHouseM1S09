e008e9af41ea6b498cced74250bcfb91ef2d30d4
Larissa <larissa.asito@gmail.com>
2022-11-30 23:38:50 -0300

    Adicionando exercício 4

--- Exercicios/src/app/app.module.ts
+++ Exercicios/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
