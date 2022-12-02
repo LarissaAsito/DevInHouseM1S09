39395f70025b216d9cea28991b253fec8667dc9c
Larissa <larissa.asito@gmail.com>
2022-12-01 22:57:37 -0300

    Adicionando o exercício 5

--- Exercicios/src/app/item-card/item-card.component.ts
+++ Exercicios/src/app/item-card/item-card.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent {
-
+  personagem: any = {
+    nome: 'Sam Gardner',
+    imagem: 'https://br.web.img3.acsta.net/c_162_216/pictures/18/01/05/14/41/2809300.jpg?    ims=704x264',
+    nomeAtor: 'Keir Gilchrist',
+    descricao:
+      'Em Atypical, Sam Gardner (Keir Gilchrist) é um jovem autista de 18 anos que está em busca de sua própria independência – começando por arrumar uma namorada.',
+  };
 }
