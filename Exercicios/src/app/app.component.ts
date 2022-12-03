fea25ded8705e99239c25fde89a2d429284b8b42
Larissa <larissa.asito@gmail.com>
2022-12-02 21:38:19 -0300

    Adicionando o exercício 6

--- Exercicios/src/app/app.component.ts
+++ Exercicios/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exercicios';
+  personagem: any = {
+    nome: 'Sam Gardner',
+    imagem: 'https://br.web.img3.acsta.net/c_162_216/pictures/18/01/05/14/41/2809300.jpg?    ims=704x264',
+    nomeAtor: 'Keir Gilchrist',
+    descricao:
+      'Em Atypical, Sam Gardner (Keir Gilchrist) é um jovem autista de 18 anos que está em busca de sua própria independência – começando por arrumar uma namorada.',
+  };
 }
--- Exercicios/src/app/item-card/item-card.component.ts
+++ Exercicios/src/app/item-card/item-card.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.css']
 })
-export class ItemCardComponent {
-  personagem: any = {
-    nome: 'Sam Gardner',
-    imagem: 'https://br.web.img3.acsta.net/c_162_216/pictures/18/01/05/14/41/2809300.jpg?    ims=704x264',
-    nomeAtor: 'Keir Gilchrist',
-    descricao:
-      'Em Atypical, Sam Gardner (Keir Gilchrist) é um jovem autista de 18 anos que está em busca de sua própria independência – começando por arrumar uma namorada.',
-  };
+export class ItemCardComponent implements OnInit{
+  @Input() nome: string = "";
+  @Input() imagem: string = "";
+  @Input() nomeAtor: string = "";
+  @Input() descricao: string = "";
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
